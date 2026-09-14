97a83b4d5d462b89c84bfac2b8f0796c6352080c
Dhanushka Madhushanka <dhanushkamg1234@gmail.com>
2026-09-14 19:00:35 +0530

    fix(ui): correct CartoDB tile layer URL for Admin Map to resolve missing tiles

--- apps/frontend/src/components/camps/AdminMap.tsx
+++ apps/frontend/src/components/camps/AdminMap.tsx
@@ -50,8 +50,8 @@ export default function AdminMap() {
         scrollWheelZoom={true}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
         />
         
         {camps.map(camp => (
